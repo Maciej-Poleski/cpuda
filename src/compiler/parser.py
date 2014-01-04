1e06b0a5e963f2ef9907b9cea64c1b4fce92a73f
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2014-01-04 19:41:33 +0100

    Improve stub.

--- src/compiler/parser.py
+++ src/compiler/parser.py
@@ -6,13 +6,41 @@ class Parser(object):
         self.m = shared_memory_definition_callback
 
     def parse(self, file):
+        result = ('extern "C"\n'
+                  '{\n'
+                  '\n'
+                  '__device__ int fun1(int* argIntStar, float argFloat)\n'
+                  '{\n'
+        )
+        result += self.m('fun1', 'tab', 'int[123]')
+        result += self.m('fun1', 'temp', 'int')
+        result += self.m('fun1', 'ptr', 'int*')
+        result += self.m('fun1', 'arr', 'int[2]')
+        result += ('\n'
+                   '    ptr=&tab[5];\n'
+                   '    tab[5]=argIntStar[threadIdx.x];\n'
+                   '    argIntStar[6]=*ptr;\n'
+                   '    return 10;\n'
+                   '}\n'
+                   '\n'
+                   '__global__ void fun2(char* argCharStar,int argInt)\n'
+                   '{\n'
+        )
+        result += self.m('fun2', 'tab', 'char[12]')
+        result += self.m('fun2', 'a', 'char')
+        result += self.m('fun2', 'b', 'char')
+        result += self.m('fun2', 'c', 'char*')
+        result += self.m('fun2', 'q', 'int')
+        result += self.m('fun2', 'w', 'int[2]')
+        result += self.m('fun2', 'e', 'int*')
+
+        result += ('\n'
+                   '}\n'
+                   '\n'
+                   '}'
+        )
+
         self.k('kern1', ('int*', 'int*', 'char'))
         self.k('kern2', ('int', 'char*'))
-        self.m('kern1', 'a', 'int*')
-        self.m('kern1', 'b', 'int[5]')
-        self.m('kern1', 'c', 'char')
-        self.m('kern2', 'd', 'int*')
-        self.m('kern2', 'e', 'int[5]')
-        self.m('kern2', 'f', 'char[1]')
 
-        return ""
+        return result
